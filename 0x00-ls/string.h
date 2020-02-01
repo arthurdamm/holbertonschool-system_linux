b4d152a42e525162c36832b9ddcada6729d139f5
Arthur Damm <indigoarthur@gmail.com>
2020-02-01 14:02:07 -0800

    Add more string helpers

--- 0x00-ls/string.h
+++ 0x00-ls/string.h
@@ -3,7 +3,11 @@
 
 #include "hls.h"
 
+/* string.c */
 int _strlen(char *s);
 char *_strdup(const char *str);
+char *_strcat(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+char *starts_with(const char *haystack, const char *needle);
 
 #endif
