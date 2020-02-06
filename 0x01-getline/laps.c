90f13ddbe6abcdeb75b48edd3000d1e5c9858631
Arthur Damm <indigoarthur@gmail.com>
2020-02-05 16:25:19 -0800

    Add racecar

---
+++ 0x01-getline/laps.c
@@ -0,0 +1,70 @@
+#include "laps.h"
+
+
+/**
+ * add_car - adds a car to linked list
+ * @head: pointer to head node
+ * @id: the car id to add
+ */
+void add_car(Car **head, int id)
+{
+	Car *node, *temp;
+
+	if (!*head)
+	{
+		node = malloc(sizeof(Car));
+		if (!node)
+			exit(EXIT_FAILURE);
+		node->id = id;
+		node->laps = 0;
+		node->next = NULL;
+		*head = node;
+		printf("Car %d joined the race\n", id);
+		return;
+	}
+	for (node = *head; node->next && node->next->id <= id; node = node->next)
+		;
+	if (node->id == id)
+	{
+		node->laps++;
+		return;
+	}
+	temp = malloc(sizeof(Car));
+	if (!temp)
+		exit(EXIT_FAILURE);
+	temp->id = id;
+	temp->laps = 0;
+	temp->next = node->next;
+	node->next = temp;
+	printf("Car %d joined the race\n", id);
+}
+
+/**
+ * race_state - determines the state
+ * @id: array of vehicle ids
+ * @size: size of above array
+ */
+void race_state(int *id, size_t size)
+{
+	static Car *head;
+	Car *node;
+	size_t i;
+
+	if (!size)
+	{
+		while (head)
+		{
+			node = head;
+			head = head->next;
+			free(node);
+		}
+		return;
+	}
+	for (i = 0; i < size; i++)
+		add_car(&head, id[i]);
+
+	printf("Race state:\n");
+	for (node = head; node; node = node->next)
+		printf("Car %d [%d laps]\n", node->id, node->laps);
+
+}
---
+++ 0x01-getline/laps.h
@@ -0,0 +1,22 @@
+#ifndef LAPS_H
+#define LAPS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct car - holds race car date
+ * @id: the race car unique id
+ * @laps: how many laps this car mae
+ * @next: next node in linked list
+ */
+typedef struct car
+{
+	int id;
+	int laps;
+	struct car *next;
+} Car;
+
+void race_state(int *id, size_t size);
+
+#endif
