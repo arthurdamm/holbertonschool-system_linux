b92f90a5e5065dc7ed95cbed9d77de7e3a78a40a
Arthur Damm <indigoarthur@gmail.com>
2020-03-28 19:05:15 +0000

    add 1-strcmp.asm

---
+++ 0x05-libasm/1-strcmp.asm
@@ -0,0 +1,39 @@
+BITS 64
+	global asm_strcmp
+	section .text
+asm_strcmp:
+	push rbp
+	mov rbp, rsp
+	push rdx
+
+	while:
+	mov rax, rdi
+	mov rdx, rsi
+	cmp BYTE [rax], 0x0
+	je after
+	cmp al, dl
+	jne after
+	inc rdi
+	inc rsi
+	jmp while
+
+	after:
+	cmp al, dl
+	je equal
+	jl less
+	mov RAX, -0x1
+	jmp end
+
+	equal:
+	mov RAX, 0x0
+	jmp end
+	less:
+	mov RAX, 0x1
+	jmp end	
+
+	end:
+	pop rdx
+
+	mov rbp, rsp
+	pop rbp
+	ret
