5fd987d1b68bffaa19d682547f1ef0f73e15bd4b
Arthur Damm <indigoarthur@gmail.com>
2020-03-28 19:05:34 +0000

    add 1-main.c

---
+++ 0x05-libasm/1-main.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "libasm.h"
+
+#define S1  "Holberton School"
+#define S2  ""
+#define S3  "Holberton Socool"
+
+/**
+ * main - Program entry point
+ *
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+int main(void)
+{
+    printf("Starting...\n");
+    printf("[%d][%d]\n", strcmp(S1, S1), asm_strcmp(S1, S1));
+    assert(strcmp(S1, S1) == asm_strcmp(S1, S1));
+    printf("[%d][%d]\n", strcmp(S1, S2), asm_strcmp(S1, S2));
+    assert(strcmp(S1, S2) == asm_strcmp(S1, S2));
+    printf("[%d][%d]\n", strcmp(S1, S3), asm_strcmp(S1, S3));
+    assert(strcmp(S1, S3) == asm_strcmp(S1, S3));
+
+    printf("All good!\n");
+    return (EXIT_SUCCESS);
+}
