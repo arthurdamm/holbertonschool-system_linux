927181c9568ab264ef329d328ddf66ccef76394d
Arthur Damm <indigoarthur@gmail.com>
2020-03-29 16:29:59 +0000

    Correct spacing remove comments

--- 0x05-libasm/0-strlen.asm
+++ 0x05-libasm/0-strlen.asm
@@ -1,12 +1,11 @@
 BITS 64
 	global asm_strlen
+
 	section .text
 
 asm_strlen:
-	; function prologue
 	push rbp
 	mov rbp, rsp
-
 	mov rax, 0
 	jmp loop_check
 
@@ -18,7 +17,6 @@ loop_check:
 	cmp byte [rdi], 0
 	jne loop_body
 
-	; function epilogue
 	mov rsp, rbp
 	pop rbp
 	ret
