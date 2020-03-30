c12e6f74c7735b0e6a0c6fd4765ba914725f4387
Arthur Damm <indigoarthur@gmail.com>
2020-03-30 05:16:45 +0000

    add 4-strstr.asm

---
+++ 0x05-libasm/4-strstr.asm
@@ -0,0 +1,58 @@
+BITS 64
+	global asm_strstr
+
+	section .text
+
+asm_strstr:
+	push rbp,
+	mov rbp, rsp
+
+	while:
+	xor rax, rax
+	cmp BYTE [rdi], 0
+	jz end
+	push rdi
+	push rsi
+	jmp _strstr
+	_strstr_return:
+	pop rsi
+	pop rdi
+	test rax, rax
+	jnz end
+	inc rdi
+	jmp while
+
+	end:
+	mov rsp, rbp
+	pop rbp
+	ret
+
+_strstr:
+	push rbp,
+	mov rbp, rsp
+
+	push rdi
+	xor rax, rax
+
+	_strstr_while:
+	movzx edx, byte [rdi]
+	movzx ecx, byte [rsi]
+	cmp dl, cl
+	after:
+	jnz _strstr_not_found
+	test cl, cl
+	jz _strstr_found
+	inc rdi
+	inc rsi
+	jmp _strstr_while
+
+	_strstr_found:
+	pop rax
+	jmp _strstr_end
+	_strstr_not_found:
+	pop rax
+	xor rax, rax
+	_strstr_end:
+	mov rsp, rbp
+	pop rbp
+	jmp _strstr_return
--- 0x05-libasm/libasm.h
+++ 0x05-libasm/libasm.h
@@ -5,6 +5,7 @@ size_t asm_strlen(const char *str);
 int asm_strcmp(const char *s1, const char *s2);
 int asm_strncmp(const char *s1, const char *s2, size_t n);
 char *asm_strchr(const char *s, int c);
+char *asm_strstr(const char *haystack, const char *needle);
 void *asm_memcpy(void *dest, const void *src, size_t n);
 size_t asm_putc(int c);
 size_t asm_puts(const char *str);
