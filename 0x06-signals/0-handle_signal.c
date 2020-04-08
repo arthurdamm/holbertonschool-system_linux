b42b30bfc4d1bd388aaa9f45f972234ea6bd4964
Arthur Damm <indigoarthur@gmail.com>
2020-04-08 03:57:23 +0000

    add 0-handle_signal.c

---
+++ 0x06-signals/0-handle_signal.c
@@ -0,0 +1,19 @@
+#include "signals.h"
+
+/**
+ * signal_handler - receives signal
+ * @sig: the signal number received
+ */
+void signal_handler(int sig)
+{
+	printf("Gotcha! [%d]\n", sig);
+}
+
+/**
+ * handle_signal - signal handler helper
+ * Return: 0 on success else -1 on error
+ */
+int handle_signal(void)
+{
+	return (signal(SIGINT, signal_handler) == SIG_ERR ? -1 : 0);
+}
---
+++ 0x06-signals/signals.h
@@ -0,0 +1,9 @@
+#ifndef SIGNALS_H
+#define SIGNALS_H
+
+#include <signal.h>
+#include <stdio.h>
+
+int handle_signal(void);
+
+#endif
