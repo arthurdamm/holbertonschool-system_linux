b929a93d182b501a6b6145f241879ca88a4ec1c0
Arthur Damm <indigoarthur@gmail.com>
2020-04-09 23:30:01 +0000

    Add task 2

---
+++ 0x06-signals/2-handle_sigaction.c
@@ -0,0 +1,22 @@
+#include "signals.h"
+
+/**
+ * signal_handler - receives signal
+ * @sig: the signal number received
+ */
+void signal_handler(int sig)
+{
+	printf("Gotcha! [%d]\n", sig);
+}
+/**
+ * handle_sigaction - applies sigaction syscall
+ * Return: 0 on success else -1
+ */
+int handle_sigaction(void)
+{
+	struct sigaction act;
+
+	memset(&act, 0, sizeof(act));
+	act.sa_handler = signal_handler;
+	return (sigaction(SIGINT, &act, NULL));
+}
