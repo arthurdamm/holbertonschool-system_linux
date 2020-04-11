2c2d6fab606932cdf3990de06145c1ff488830ec
Arthur Damm <indigoarthur@gmail.com>
2020-04-11 03:58:29 +0000

    add signals.h

--- 0x06-signals/signals.h
+++ 0x06-signals/signals.h
@@ -15,5 +15,6 @@ void (*current_handler_signal(void))(int);
 int handle_sigaction(void);
 void (*current_handler_sigaction(void))(int);
 int trace_signal_sender(void);
+int pid_exist(pid_t pid);
 
 #endif
