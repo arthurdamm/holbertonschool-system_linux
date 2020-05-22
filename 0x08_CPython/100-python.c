1b7e3d460191b36a2613805fd5afe892583c0b61
Arthur Damm <indigoarthur@gmail.com>
2020-05-22 22:19:34 +0000

    Initial refactor to remove code

--- 0x08_CPython/100-python.c
+++ 0x08_CPython/100-python.c
@@ -1,45 +1,30 @@
 #include <Python.h>
 
-int long_to_decimal_string_internal(PyObject *aa,
-                                PyObject **p_output,
-                                char **bytes_str)
+int long_to_decimal_string_internal(PyObject *aa, char **bytes_str)
 {
-    PyLongObject *scratch, *a;
-    PyObject *str = NULL;
+    PyLongObject *a;
+    char *str = NULL;
     Py_ssize_t size, strlen, size_a, i, j;
     digit *pout, *pin, rem, tenpow;
     int negative;
     int d;
-    enum PyUnicode_Kind kind;
 
     a = (PyLongObject *)aa;
     size_a = Py_SIZE(a) < 0 ? -Py_SIZE(a) : Py_SIZE(a);
     negative = Py_SIZE(a) < 0;
 
-    /* quick and dirty upper bound for the number of digits
-       required to express a in base _PyLong_DECIMAL_BASE:
-         #digits = 1 + floor(log2(a) / log2(_PyLong_DECIMAL_BASE))
-       But log2(a) < size_a * PyLong_SHIFT, and
-       log2(_PyLong_DECIMAL_BASE) = log2(10) * _PyLong_DECIMAL_SHIFT
-                                  > 3.3 * _PyLong_DECIMAL_SHIFT
-         size_a * PyLong_SHIFT / (3.3 * _PyLong_DECIMAL_SHIFT) =
-             size_a + size_a / d < size_a + size_a / floor(d),
-       where d = (3.3 * _PyLong_DECIMAL_SHIFT) /
-                 (PyLong_SHIFT - 3.3 * _PyLong_DECIMAL_SHIFT)
-    */
     d = (33 * _PyLong_DECIMAL_SHIFT) /
         (10 * PyLong_SHIFT - 33 * _PyLong_DECIMAL_SHIFT);
     /* assert(size_a < PY_SSIZE_T_MAX/2); */
     size = 1 + size_a + size_a / d;
-    scratch = _PyLong_New(size);
-    if (scratch == NULL)
-        return -1;
+	pout = calloc(sizeof(digit), size);
+    if (!pout)
+		return (-1);
 
     /* convert array of base _PyLong_BASE digits in pin to an array of
        base _PyLong_DECIMAL_BASE digits in pout, following Knuth (TAOCP,
        Volume 2 (3rd edn), section 4.4, Method 1b). */
     pin = a->ob_digit;
-    pout = scratch->ob_digit;
     size = 0;
     for (i = size_a; --i >= 0; ) {
         digit hi = pin[i];
@@ -68,14 +53,9 @@ int long_to_decimal_string_internal(PyObject *aa,
         strlen++;
     }
 
-    {
-        str = PyUnicode_New(strlen, '9');
-        if (str == NULL) {
-            Py_DECREF(scratch);
-            return -1;
-        }
-        kind = PyUnicode_KIND(str);
-    }
+	str = calloc(1, strlen + 1);
+	if (!str)
+		return -1;
 
 #define WRITE_DIGITS(p)                                               \
     do {                                                              \
@@ -100,48 +80,29 @@ int long_to_decimal_string_internal(PyObject *aa,
             *--p = '-';                                               \
     } while (0)
 
-#define WRITE_UNICODE_DIGITS(TYPE)                                    \
+#define WRITE_UNICODE_DIGITS                                    \
     do {                                                              \
-        p = (TYPE*)PyUnicode_DATA(str) + strlen;                  \
-                                                                      \
+        p = str + strlen;                  \
         WRITE_DIGITS(p);                                              \
-                                                                      \
-        /* check we've counted correctly */                           \
-        assert(p == (TYPE*)PyUnicode_DATA(str));                  \
     } while (0)
 
     /* fill the string right-to-left */
 
-    if (kind == PyUnicode_1BYTE_KIND) {
-        Py_UCS1 *p;
-        WRITE_UNICODE_DIGITS(Py_UCS1);
-    }
-    else if (kind == PyUnicode_2BYTE_KIND) {
-        Py_UCS2 *p;
-        WRITE_UNICODE_DIGITS(Py_UCS2);
-    }
-    else {
-        Py_UCS4 *p;
-        assert (kind == PyUnicode_4BYTE_KIND);
-        WRITE_UNICODE_DIGITS(Py_UCS4);
-    }
+        char *p;
+        WRITE_UNICODE_DIGITS;
+
 #undef WRITE_DIGITS
 #undef WRITE_UNICODE_DIGITS
 
-    Py_DECREF(scratch);
-    {
-        *p_output = (PyObject *)str;
-    }
+	printf("%s\n", str);
     return 0;
 	(void)bytes_str;
 }
 
-PyObject *
-print_python_int(PyObject *aa)
+void print_python_int(PyObject *aa)
 {
-    PyObject *v;
-    if (long_to_decimal_string_internal(aa, &v, NULL) == -1)
-        return NULL;
-	wprintf(L"%ls\n", PyUnicode_AS_UNICODE(v));
-    return v;
+    if (long_to_decimal_string_internal(aa, NULL) == -1)
+        return;
+	// wprintf(L"%ls\n", PyUnicode_AS_UNICODE(v));
+    return;
 }
