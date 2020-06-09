1dd9ac282c581025c314fb77434c1a11de12d0d1
Arthur Damm <indigoarthur@gmail.com>
2020-06-09 15:07:18 +0000

    Add task 0

---
+++ 0x09-strace/strace.h
@@ -0,0 +1,14 @@
+#ifndef STRACE_H
+#define STRACE_H
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ptrace.h>
+#include <sys/wait.h>
+#include <x86_64-linux-gnu/sys/user.h>
+
+#include "syscall_array.h"
+
+#endif
