02acf4f4ea6130441e92d4c90d932f76f4a35c4b
Arthur Damm <indigoarthur@gmail.com>
2020-06-10 19:12:43 +0000

    add strace_2.c

---
+++ 0x09-strace/strace_2.c
@@ -0,0 +1,100 @@
+#include "strace.h"
+
+void trace_child(char **av, char **envp);
+void trace_parent(pid_t child_pid);
+int await_syscall(pid_t child_pid);
+
+/**
+ * main - entry point
+ * @ac: argument count
+ * @av: argument vector
+ * @envp: environ
+ * Return: EXIT_SUCCESS or error.
+ */
+int main(int ac, char **av, char **envp)
+{
+	pid_t child_pid;
+
+	if (ac < 2)
+	{
+		printf("Usage: %s command [args...]\n", av[0]);
+		return (EXIT_FAILURE);
+	}
+	setbuf(stdout, NULL);
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		dprintf(STDERR_FILENO, "Fork failed: %d\n", errno);
+		exit(-1);
+	}
+	else if (!child_pid)
+		trace_child(av, envp);
+	else
+		trace_parent(child_pid);
+	return (0);
+}
+
+/**
+ * trace_child - traces child process
+ * @av: argument vector for execve
+ * @envp: environ for execve
+ */
+void trace_child(char **av, char **envp)
+{
+	ptrace(PTRACE_TRACEME, 0, 0, 0);
+	kill(getpid(), SIGSTOP);
+	if (execve(av[1], av + 1, envp) == -1)
+	{
+		dprintf(STDERR_FILENO, "Exec failed: %d\n", errno);
+		exit(-1);
+	}
+}
+
+/**
+ * trace_parent - calls made by tracing parent
+ * @child_pid: pid of child to trace
+ */
+void trace_parent(pid_t child_pid)
+{
+	int status;
+	struct user_regs_struct uregs;
+
+	waitpid(child_pid, &status, 0);
+	ptrace(PTRACE_SETOPTIONS, child_pid, 0, PTRACE_O_TRACESYSGOOD);
+	while (1)
+	{
+		if (await_syscall(child_pid))
+			break;
+		memset(&uregs, 0, sizeof(uregs));
+		ptrace(PTRACE_GETREGS, child_pid, 0, &uregs);
+		printf("%s", syscalls_64_g[uregs.orig_rax].name);
+		if (await_syscall(child_pid))
+			break;
+		memset(&uregs, 0, sizeof(uregs));
+		ptrace(PTRACE_GETREGS, child_pid, 0, &uregs);
+		printf(" = %#lx\n", (long)uregs.rax);
+	}
+}
+
+/**
+ * await_syscall - waits for a syscall
+ * @child_pid: pid of process to await
+ * Return: 0 if child stopped, 1 if exited
+ */
+int await_syscall(pid_t child_pid)
+{
+	int status;
+
+	while (1)
+	{
+		ptrace(PTRACE_SYSCALL, child_pid, 0, 0);
+		waitpid(child_pid, &status, 0);
+		if (WIFSTOPPED(status) && WSTOPSIG(status) & 0x80)
+			return (0);
+		if (WIFEXITED(status))
+		{
+			printf(" = ?\n");
+			return (1);
+		}
+	}
+}
