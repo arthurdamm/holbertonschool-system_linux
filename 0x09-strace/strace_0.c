2f80be8fe59498b6529aef25e0370a782ca3b591
Arthur Damm <indigoarthur@gmail.com>
2020-06-10 18:58:04 +0000

    Refactor based on blueprint

--- 0x09-strace/strace_0.c
+++ 0x09-strace/strace_0.c
@@ -1,6 +1,8 @@
 #include "strace.h"
 
 void trace_child(char **av, char **envp);
+void trace_parent(pid_t child_pid);
+int await_syscall(pid_t child_pid);
 
 /**
  * main - entry point
@@ -12,9 +14,6 @@ void trace_child(char **av, char **envp);
 int main(int ac, char **av, char **envp)
 {
 	pid_t child_pid;
-	int status = 0;
-	struct user_regs_struct uregs;
-	unsigned long last_syscall = 0;
 
 	if (ac < 2)
 	{
@@ -29,28 +28,11 @@ int main(int ac, char **av, char **envp)
 		exit(-1);
 	}
 	else if (!child_pid)
-	{
 		trace_child(av, envp);
-	}
-	ptrace(PTRACE_SETOPTIONS, child_pid, 0, PTRACE_O_TRACESYSGOOD);
-	do {
-		ptrace(PTRACE_SYSCALL, child_pid, 0, 0);
-		wait(&status);
-		memset(&uregs, 0, sizeof(uregs));
-		ptrace(PTRACE_GETREGS, child_pid, 0, &uregs);
-		if (last_syscall)
-		{
-			last_syscall = 0;
-		}
-		else
-		{
-			last_syscall = uregs.orig_rax;
-			printf("%lu\n", last_syscall);
-		}
-	} while (!WIFEXITED(status));
+	else
+		trace_parent(child_pid);
 	return (0);
 }
-
 
 /**
  * trace_child - traces child process
@@ -60,9 +42,53 @@ int main(int ac, char **av, char **envp)
 void trace_child(char **av, char **envp)
 {
 	ptrace(PTRACE_TRACEME, 0, 0, 0);
+	kill(getpid(), SIGSTOP);
 	if (execve(av[1], av + 1, envp) == -1)
 	{
 		dprintf(STDERR_FILENO, "Exec failed: %d\n", errno);
 		exit(-1);
+	}
+}
+
+/**
+ * trace_parent - calls made by tracing parent
+ * @child_pid: pid of child to trace
+ */
+void trace_parent(pid_t child_pid)
+{
+	int status;
+	struct user_regs_struct uregs;
+
+	waitpid(child_pid, &status, 0);
+	ptrace(PTRACE_SETOPTIONS, child_pid, 0, PTRACE_O_TRACESYSGOOD);
+	while (1)
+	{
+		if (await_syscall(child_pid))
+			break;
+		memset(&uregs, 0, sizeof(uregs));
+		ptrace(PTRACE_GETREGS, child_pid, 0, &uregs);
+		printf("%lu\n", (long)uregs.orig_rax);
+		if (await_syscall(child_pid))
+			break;
+	}
+}
+
+/**
+ * await_syscall - waits for a syscall
+ * @child_pid: pid of process to await
+ * Return: 0 if child stopped, 1 if exited
+ */
+int await_syscall(pid_t child_pid)
+{
+	int status;
+
+	while (1)
+	{
+		ptrace(PTRACE_SYSCALL, child_pid, 0, 0);
+		waitpid(child_pid, &status, 0);
+		if (WIFSTOPPED(status) && WSTOPSIG(status) & 0x80)
+			return (0);
+		if (WIFEXITED(status))
+			return (1);
 	}
 }
